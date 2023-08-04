010d6570e7cb4aab2e8460c9861538f23dc19ebf
Idin <idin@mac.home>
2023-08-04 09:10:01 -0400

    feat: Add path utilities for consistent directory access

--- gmailwiz/utils/__init__.py
+++ gmailwiz/utils/__init__.py
@@ -1,13 +1,19 @@
 """
-GmailWiz utility functions.
+Utilities module for GmailWiz.
 
-This module contains progress tracking, CLI, and other utility functions.
+This module contains utility functions and helper classes.
 """
 
 from .progress import EmailProgressTracker
+from .email_lists import EmailListManager
 from .query_builder import build_gmail_search_query
+from .paths import (
+    get_package_root, get_core_dir, get_analysis_dir, get_utils_dir, 
+    get_cache_dir, get_project_root, get_tests_dir, verify_package_structure
+)
 
 __all__ = [
-    "EmailProgressTracker",
-    "build_gmail_search_query"
+    'EmailProgressTracker', 'EmailListManager', 'build_gmail_search_query',
+    'get_package_root', 'get_core_dir', 'get_analysis_dir', 'get_utils_dir',
+    'get_cache_dir', 'get_project_root', 'get_tests_dir', 'verify_package_structure'
 ]
---
+++ gmailwiz/utils/paths.py
@@ -0,0 +1,130 @@
+"""
+Path utilities for GmailWiz package.
+
+This module provides utility functions to get package directories and paths
+that can be used by tests and other modules.
+"""
+
+from pathlib import Path
+import os
+
+
+def get_package_root() -> Path:
+    """
+    Get the root directory of the GmailWiz package.
+    
+    Returns:
+        Path: Path to the gmailwiz package root directory.
+    """
+    # Get the directory containing this file
+    current_file = Path(__file__)
+    # Go up to utils, then up to gmailwiz root
+    return current_file.parent.parent
+
+
+def get_core_dir() -> Path:
+    """
+    Get the core module directory.
+    
+    Returns:
+        Path: Path to the gmailwiz/core directory.
+    """
+    return get_package_root() / 'core'
+
+
+def get_analysis_dir() -> Path:
+    """
+    Get the analysis module directory.
+    
+    Returns:
+        Path: Path to the gmailwiz/analysis directory.
+    """
+    return get_package_root() / 'analysis'
+
+
+def get_utils_dir() -> Path:
+    """
+    Get the utils module directory.
+    
+    Returns:
+        Path: Path to the gmailwiz/utils directory.
+    """
+    return get_package_root() / 'utils'
+
+
+def get_cache_dir() -> Path:
+    """
+    Get the cache module directory.
+    
+    Returns:
+        Path: Path to the gmailwiz/cache directory.
+    """
+    return get_package_root() / 'cache'
+
+
+def get_project_root() -> Path:
+    """
+    Get the project root directory (parent of gmailwiz package).
+    
+    Returns:
+        Path: Path to the project root directory.
+    """
+    return get_package_root().parent
+
+
+def get_tests_dir() -> Path:
+    """
+    Get the tests directory.
+    
+    Returns:
+        Path: Path to the tests directory.
+    """
+    return get_project_root() / 'tests'
+
+
+def verify_package_structure() -> dict:
+    """
+    Verify that all required package directories and files exist.
+    
+    Returns:
+        dict: Dictionary with verification results for each module.
+    """
+    results = {}
+    
+    # Check core module
+    core_dir = get_core_dir()
+    core_files = ['__init__.py', 'gmail.py', 'gmail_client.py', 'config.py']
+    results['core'] = {
+        'dir_exists': core_dir.exists(),
+        'files_exist': all((core_dir / file).exists() for file in core_files),
+        'missing_files': [file for file in core_files if not (core_dir / file).exists()]
+    }
+    
+    # Check analysis module
+    analysis_dir = get_analysis_dir()
+    analysis_files = ['__init__.py', 'email_analyzer.py', 'email_metrics.py', 'metrics_processor.py']
+    results['analysis'] = {
+        'dir_exists': analysis_dir.exists(),
+        'files_exist': all((analysis_dir / file).exists() for file in analysis_files),
+        'missing_files': [file for file in analysis_files if not (analysis_dir / file).exists()]
+    }
+    
+    # Check utils module
+    utils_dir = get_utils_dir()
+    utils_files = ['__init__.py', 'progress.py', 'cli.py', 'email_lists.py', 'query_builder.py']
+    results['utils'] = {
+        'dir_exists': utils_dir.exists(),
+        'files_exist': all((utils_dir / file).exists() for file in utils_files),
+        'missing_files': [file for file in utils_files if not (utils_dir / file).exists()]
+    }
+    
+    # Check cache module
+    cache_dir = get_cache_dir()
+    cache_files = ['__init__.py', 'cache_config.py', 'cache_manager.py', 'file_storage.py', 'index_manager.py', 'schema_manager.py']
+    results['cache'] = {
+        'dir_exists': cache_dir.exists(),
+        'files_exist': all((cache_dir / file).exists() for file in cache_files),
+        'missing_files': [file for file in cache_files if not (cache_dir / file).exists()]
+    }
+    
+    return results
